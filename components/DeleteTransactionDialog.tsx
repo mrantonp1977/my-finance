8cee428d4474a37ec536d2b6471560034772eb4f
Antonis Papaioannou <mrantonp@gmail.com>
2025-08-17 21:47:38 +0300

    feat: implement DeleteTransaction functionality, create DeleteTransactionDialog component, and integrate with TransactionTable

---
+++ components/DeleteTransactionDialog.tsx
@@ -0,0 +1,62 @@
+"use client"
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "./ui/alert-dialog";
+import { DeleteTransaction } from "@/app/(dashboard)/transactions/_actions/deleteTransaction";
+
+interface Props {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  transactionId: string;
+}
+
+const DeleteTransactionDialog = ({ open, setOpen,  transactionId }: Props) => {
+  const queryClient = useQueryClient();
+
+  const deleteMutation = useMutation({
+    mutationFn: DeleteTransaction,
+    onSuccess: async () => {
+        toast.success('Transaction deleted successfully!', {
+          id: transactionId,
+        });
+        await queryClient.invalidateQueries({
+          queryKey: ["transactions"]
+        });
+    },
+    onError: () => {
+      toast.error("Something went wrong", {
+        id: transactionId
+      });
+    },
+  });
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Are you absolutely sure?
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            This action can not be undone! This will permanently delete your transaction.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>
+            Cancel
+          </AlertDialogCancel>
+          <AlertDialogAction onClick={() => {
+            toast.loading('Deleting transaction...', {
+              id: transactionId
+            });
+            deleteMutation.mutate(transactionId)
+          }}>
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
+
+export default DeleteTransactionDialog
--- components/TransactionTable.tsx
+++ components/TransactionTable.tsx
@@ -5,8 +5,11 @@ import { DateToUTCDate } from '@/lib/helpers';
 import { useQuery } from '@tanstack/react-query';
 import {
   ColumnDef,
+  ColumnFiltersState,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
   getSortedRowModel,
   SortingState,
   useReactTable,
@@ -22,8 +25,22 @@ import {
 } from '@/components/ui/table';
 import SkeletonWrapper from './SkeletonWrapper';
 import { DataTableColumnHeader } from './ColumnHeader';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { cn } from '@/lib/utils';
+import { DataTableFacetedFilter } from './FacetedFilters';
+import { DataTableViewOptions } from './ColumnToggle';
+import { Button } from './ui/button';
+import { download, generateCsv, mkConfig } from 'export-to-csv';
+import { DownloadIcon, MoreHorizontal, TrashIcon } from 'lucide-react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+import DeleteTransactionDialog from './DeleteTransactionDialog';
 
 interface Props {
   from: Date;
@@ -35,12 +52,15 @@ const emptyData: any[] = [];
 
 type TransactionHistoryRow = GetTransactionHistoryResponseType[0];
 
-export const columns: ColumnDef<TransactionHistoryRow>[] = [
+const columns: ColumnDef<TransactionHistoryRow>[] = [
   {
     accessorKey: 'category',
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="Category" />
     ),
+    filterFn: (row, id, value) => {
+      return value.includes(row.getValue(id));
+    },
     cell: ({ row }) => (
       <div className="flex gap-2 capitalize">
         {row.original.categoryIcon}
@@ -81,9 +101,8 @@ export const columns: ColumnDef<TransactionHistoryRow>[] = [
         className={cn(
           'capitalize rounded-lg text-center p-2',
           row.original.type === 'income' &&
-            'border border-emerald-500 text-emerald-500',
-          row.original.type === 'expense' &&
-            'border border-rose-500 text-rose-500'
+            'bg-emerald-400/10 text-emerald-500',
+          row.original.type === 'expense' && 'bg-rose-400/10 text-rose-500'
         )}
       >
         {row.original.type}
@@ -101,10 +120,23 @@ export const columns: ColumnDef<TransactionHistoryRow>[] = [
       </p>
     ),
   },
+  {
+    id: 'actions',
+    enableHiding: false,
+    cell: ({ row }) => <RowActions transaction={row.original} />,
+  },
 ];
+
+const csvConfig = mkConfig({
+  fieldSeparator: ',',
+  decimalSeparator: '.',
+  useKeysAsHeaders: true,
+});
 
 const TransactionTable = ({ from, to }: Props) => {
   const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+
   const history = useQuery<GetTransactionHistoryResponseType>({
     queryKey: ['transactions', 'history', from, to],
     queryFn: () =>
@@ -115,19 +147,80 @@ const TransactionTable = ({ from, to }: Props) => {
       ).then((res) => res.json()),
   });
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleExportCSV = (data: any[]) => {
+    const csv = generateCsv(csvConfig)(data);
+    download(csvConfig)(csv);
+  };
+
   const table = useReactTable({
     data: history.data || emptyData,
     columns,
     getCoreRowModel: getCoreRowModel(),
-    state: { sorting },
+    state: { sorting, columnFilters },
     onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
     getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
   });
+
+  const categoriesOptions = useMemo(() => {
+    const categoriesMap = new Map();
+    history.data?.forEach((transaction) => {
+      categoriesMap.set(transaction.category, {
+        value: transaction.category,
+        label: `${transaction.categoryIcon} ${transaction.category}`,
+      });
+    });
+    const uniqueCategories = new Set(categoriesMap.values());
+    return Array.from(uniqueCategories);
+  }, [history.data]);
 
   return (
     <div className="w-full">
       <div className="flex flex-wrap items-end justify-between gap-2 py-4">
-        TODO: Filters
+        <div className="flex gap-2">
+          {table.getColumn('category') && (
+            <DataTableFacetedFilter
+              title="Category"
+              column={table.getColumn('category')}
+              options={categoriesOptions}
+            />
+          )}
+          {table.getColumn('type') && (
+            <DataTableFacetedFilter
+              title="Type"
+              column={table.getColumn('type')}
+              options={[
+                { label: 'Income', value: 'income' },
+                { label: 'Expense', value: 'expense' },
+              ]}
+            />
+          )}
+        </div>
+        <div className="flex flex-wrap gap-2">
+          <Button
+            size={'sm'}
+            className="ml-auto mx-4 h-8 lg:flex"
+            onClick={() => {
+              const data = table.getFilteredRowModel().rows.map((row) => ({
+                category: row.original.category,
+                categoryIcon: row.original.categoryIcon,
+                description: row.original.description,
+                type: row.original.type,
+                amount: row.original.amount,
+                formattedAmount: row.original.formattedAmount,
+                date: row.original.date,
+              }));
+              handleExportCSV(data);
+            }}
+          >
+            <DownloadIcon className="mr-2" />
+            Export CSV
+          </Button>
+          <DataTableViewOptions table={table} />
+        </div>
       </div>
       <SkeletonWrapper isLoading={history.isFetching}>
         <div className="overflow-hidden rounded-md border">
@@ -180,9 +273,58 @@ const TransactionTable = ({ from, to }: Props) => {
             </TableBody>
           </Table>
         </div>
+        <div className="flex items-center justify-end space-x-2 py-4">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
       </SkeletonWrapper>
     </div>
   );
 };
 
 export default TransactionTable;
+
+function RowActions({ transaction }: { transaction: TransactionHistoryRow }) {
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+
+  return (
+    <>
+    <DeleteTransactionDialog open={showDeleteDialog} setOpen={setShowDeleteDialog} transactionId={transaction.id}/>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant={'ghost'} className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            className="flex items-center gap-2"
+            onSelect={() => {
+              setShowDeleteDialog((prev) => !prev);
+            }}
+          >
+            <TrashIcon className="ml-2" />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+}
